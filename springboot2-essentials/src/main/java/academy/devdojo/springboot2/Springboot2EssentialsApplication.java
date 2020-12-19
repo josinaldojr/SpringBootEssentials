f80b5ba62711f32e8ef2537168c0ede2d1aa92e3
Josinaldo Júnior <josinaldo.pontes@dcx.ufpb.br>
2020-12-19 02:13:48 -0300

    feat(#5): create conection mysql and docker-compose

--- springboot2-essentials/src/main/java/academy/devdojo/springboot2/Springboot2EssentialsApplication.java
+++ springboot2-essentials/src/main/java/academy/devdojo/springboot2/Springboot2EssentialsApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Springboot2EssentialsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot2EssentialsApplication.class, args);
 	}
-
 }
+
