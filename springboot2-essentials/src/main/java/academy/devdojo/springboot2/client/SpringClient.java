f944e26443efbf0b33508e49d0d0a444eb16781f
Josinaldo Júnior <josinaldo.pontes@dcx.ufpb.br>
2021-01-07 15:24:08 -0300

    feat(#20): create other routes for method GET not pageable

--- springboot2-essentials/src/main/java/academy/devdojo/springboot2/client/SpringClient.java
+++ springboot2-essentials/src/main/java/academy/devdojo/springboot2/client/SpringClient.java
@@ -2,8 +2,13 @@ package academy.devdojo.springboot2.client;
 
 import academy.devdojo.springboot2.domain.Anime;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Log4j2
 public class SpringClient {
@@ -11,8 +16,17 @@ public class SpringClient {
         ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8080/animes/{id}", Anime.class, 2);
         log.info(entity);
 
-        Anime object = new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Anime.class, 2);
+        Anime[] animes = new RestTemplate().getForObject("http://localhost:8080/animes/all", Anime[].class);
 
-        log.info(object);
+        log.info(Arrays.toString(animes));
+
+        ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange("http://localhost:8080/animes/all",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Anime>>() {
+                });
+
+        log.info(exchange.getBody());
+
     }
 }
